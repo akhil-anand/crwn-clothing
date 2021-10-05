226e8823a9dbaa0be11a4ebebce87b43ed475285
akhil-code1 <code1@phicode.io>
2021-10-05 20:13:40 +0530

    adding header component and links to home, shop page and contact

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import logo from './logo.svg';
+//Lib imports
 import './App.css';
-import { Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
+
+//Component imports
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 // const HatsPage = () => (
 //   <div>
@@ -13,8 +16,11 @@ import ShopPage from './pages/shop/shop.component';
 function App() {
   return (
     <div>
+      <Header/>
+      <Switch>
       <Route exact path='/' component={HomePage}/>
-      <Route exact path='/shop' component={ShopPage}/>
+      <Route path='/shop' component={ShopPage}/>
+      </Switch>
       {/* <Route exact path='/hats' component={HatsPage}/>*/}
       {/* <HomePage /> */}
     </div>
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,25 @@
+//Lib imports
+import React from "react";
+import {Link} from "react-router-dom";
+
+//Component imports
+import './header.styles.scss';
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+
+const Header = () => (
+    <div className="header">
+        <Link className='logo-container' to='/'>
+            <Logo className='logo' />
+        </Link>
+        <div className="options">
+            <Link className="option" to='/shop'>
+                SHOP
+            </Link>
+            <Link className="option" to='/shop'>
+                CONTACT
+            </Link>          
+        </div>
+    </div>
+)
+
+export default Header;
