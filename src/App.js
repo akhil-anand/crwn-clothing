c0c50395295a68fd3fd8bcdcf0089b369691c6a3
akhil-code1 <code1@phicode.io>
2021-10-05 18:35:29 +0530

    used with Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Route } from 'react-router-dom'
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path='/' component={HomePage}/>
+      <Route exact path='/hats' component={HatsPage}/>
+      {/* <HomePage /> */}
     </div>
   );
 }
